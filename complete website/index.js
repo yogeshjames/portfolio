fbdcf668f37100d637d864f63791d954f0c14470
yogeshjames <150313150+yogeshjames@users.noreply.github.com>
2024-01-25 23:31:42 +0530

    Add files via upload

---
+++ complete website/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/jjk", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/fuckyou", (req, res) => {
+  res.render("about.ejs");
+});
+app.get("/mff", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.use(express.static("public"));
+
+app.post("/contact", (req, res) => {
+  res.render("index.ejs", { san : req.body.name});
+ // console.log(req.body);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
